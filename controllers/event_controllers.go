002725f2dc62490511aad564835348c0d4278053
Reyy112 <141012102+Reyy112@users.noreply.github.com>
2026-07-29 01:23:57 +0700

    feat: add CRUD routes and handlers for events

---
+++ controllers/event_controllers.go
@@ -0,0 +1,102 @@
+package controllers
+
+import (
+	"example/event-app/config"
+	"example/event-app/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateEvents(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	event.UserId = 1
+	config.DB.Create(&event)
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "Data Created",
+		"event":   event,
+	})
+}
+
+func GetEvents(context *gin.Context) {
+	var event []models.Event
+
+	config.DB.Find(&event)
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Data Showing All",
+		"event":   event,
+	})
+}
+
+func GetEventsbyId(context *gin.Context) {
+	var event models.Event
+	paramsId := context.Param("id")
+
+	var eventData = config.DB.First(&event, paramsId).Error
+	if eventData != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Event not found",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Data Showing Detail Event",
+		"event":   event,
+	})
+}
+
+func UpdateEvent(context *gin.Context) {
+	var event models.Event
+	paramsId := context.Param("id")
+
+	var eventData = config.DB.First(&event, paramsId).Error
+	if eventData != nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"error": "Event not found",
+		})
+		return
+	}
+
+	var input models.Event
+	err := context.ShouldBindJSON(&input)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	config.DB.Model(&event).Updates(input)
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Data get update",
+		"event":   event,
+	})
+}
+
+func DeleteEvent(context *gin.Context) {
+	var event models.Event
+	paramId := context.Param("id")
+
+	var eventData = config.DB.First(&event, paramId).Error
+	if eventData != nil {
+		context.JSON(http.StatusNotFound, gin.H{
+			"error": "Event not found",
+		})
+		return
+	}
+
+	config.DB.Unscoped().Delete(&event)
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Data success to Deleted",
+		"event":   event,
+	})
+}
--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"example/event-app/config"
-	"example/event-app/models"
+	"example/event-app/controllers"
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -19,42 +18,16 @@ func main() {
 	config.ConnectDB()
 
 	server := gin.Default()
-	// server port
-	server.Run(":8080")
 	// router
 	api := server.Group("/api")
 	{
-		api.POST("/events", createEvents)
-		api.GET("/events", getEvents)
-	}
-}
-
-// Function Handler
-// createEvents
-func createEvents(context *gin.Context) {
-	var events models.Event
-	err := context.ShouldBindJSON(&events)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{
-			"message": "Invalid Request",
-			"error":   err.Error(),
-		})
-		return
+		api.POST("/events", controllers.CreateEvents)
+		api.GET("/events", controllers.GetEvents)
+		api.GET("/events/:id", controllers.GetEventsbyId)
+		api.PUT("/events/:id", controllers.UpdateEvent)
+		api.DELETE("/events/:id", controllers.DeleteEvent)
 	}
 
-	events.UserId = 1
-
-	// save input
-	events.Save()
-	context.JSON(http.StatusCreated, gin.H{
-		"message": "Event Created",
-		"events":  events,
-	})
-}
-
-// getEvents
-func getEvents(context *gin.Context) {
-	events := models.GetAllEvent()
-	context.JSON(http.StatusOK, events)
+	// server port
+	server.Run(":8080")
 }
